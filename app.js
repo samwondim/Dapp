7862340aa23114c62a0d7c7dbd67da92ac36b701
samwondim <samwondim@gmail.com>
2024-03-25 00:04:47 +0300

    add basic folder structure for subsystems decomp

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 const express = require("express")
-const firebase = require("firebase")
 
+// Import routes from the subsystems.
+const userRoutes = require('./userAccountManagement/userRoutes')
+const messagingRoutes = require('./messaging/messageRoutes')
+const communicationRoutes = require('./communication/communicationRoutes')
+const discoveryRoutes = require('./userDiscovery/discoveryRoutes')
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  domainURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-}
+  apiKey: "AIzaSyB__J8Zdf5Ajjl-F92fs3q45UDAES51tnw",
+  authDomain: "elective-ii-project.firebaseapp.com",
+  projectId: "elective-ii-project",
+  storageBucket: "elective-ii-project.appspot.com",
+  messagingSenderId: "671063354670",
+  appId: "1:671063354670:web:b3d798c46e011eb1f31ebf",
+  measurementId: "G-FCNM0YP3ZZ"
+};
 
-firebase.initializeApp(firebaseConfig)
+// Initialize Firebase
+const firebase_app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const app = express()
 const port = 3000
+
+app.use('/users', userRoutes)
+app.use('/discovery', discoveryRoutes)
+app.use('/communication', communicationRoutes)
+app.use('/message', messagingRoutes)
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`)
